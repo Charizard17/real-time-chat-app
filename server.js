9d5f60fd901f090ca14bfff0639f06ebeb852bdd
Esad Dursun <3723esad@gmail.com>
2021-02-25 09:54:12 +0100

    send message client to server and broadcast it to other users

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const io = require('socket.io')(3000, {
 });
 
 io.on('connection', socket => {
-    console.log('new User');
-    socket.emit('chat-message', 'Hello World')
+    socket.emit('chat-message', 'Hello World');
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message);
+    })
 }); 
